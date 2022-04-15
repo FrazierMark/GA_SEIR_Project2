c61125296623f02bc2fd0df9a4db14bdf1a7c10a
FrazierMark <m.mark.frazier@gmail.com>
2022-04-14 19:53:56 -0700

    Path change

--- server.js
+++ server.js
@@ -38,12 +38,13 @@ app.use(passport.session());
 app.use(logger('dev'));
 app.use(methodOverride('_method'));
 app.use(express.json());
+app.use('/public/', express.static(path.join(__dirname, 'public')));
 //app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(cookieParser());
-app.use(express.static('public'));
+
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 app.use('/dist/', express.static(path.join(__dirname, 'node_modules/lil-gui/dist')));
